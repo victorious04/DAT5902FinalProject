0a35db02b8946eff58afcadd30c9ffdd69d4ae17
victorious04 <victoriaadegbenla@gmail.com>
2025-01-04 01:00:52 +0000

    updating of functions, adding of comments and slight fixing of import

--- DAT5902Functions.py
+++ DAT5902Functions.py
@@ -1,6 +1,6 @@
 ###importing possibly necessary python libraries###
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
 ###Functions that may be used often/speed up process
@@ -20,10 +20,12 @@ def mergeData(dataSet1, dataSet2,mergeCols,type):
 
 #creation of a scatter plot
 def createScatterPlot(dataSet,xdata,ydata,title,xlabel,ylabel):
-    plt.scatter(dataSet[xdata], dataSet[ydata])
+    plt.figure(figsize=(10,6))
+    plt.scatter(dataSet[xdata], dataSet[ydata], alpha=0.6, edgecolor='black')
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
+    plt.grid(True)
     plt.show()
     return
 
@@ -32,7 +34,12 @@ def saveVisual(outputpath):
     plt.savefig(outputpath)
     return
 
+#dropping of unneeded columns in df
 def dropColumns(dataSet, dropCols):
     dataSet = dataSet.drop(columns=dropCols)
     return dataSet
-##END OF FUNCTIONS
+
+#selection of a specific year to focus on
+def yearSelection(dataSet, year):
+    dataSet = dataSet[dataSet['Year'] == year]
+    return dataSet
--- DAT5902Visuals.py
+++ DAT5902Visuals.py
@@ -1,10 +1,10 @@
 ###importing possibly necessary python libraries###
-import pandas as pd
-import matplotlib as plt
-import numpy as np
+#because there is now a separate file which contains all functions, by importing everything in there
+#there is no need for libraries such as numpy and matplotlib as this 'main; file isn't doing anything that woul
+#need those libraries, it's all being done in the functions file.
 from DAT5902Functions import *
 
-
+#importing of necessary data
 lifeDf = importDF('life-expectancy.csv')
 gdpDF = importDF('gdp-per-capita-worldbank.csv')
 
@@ -21,4 +21,11 @@ print(dfHead(life_V_gdpDF))
 life_V_gdpDF = dropColumns(life_V_gdpDF,['Code_x','Code_y'])
 print(dfHead(life_V_gdpDF))
 
-#now to create the
+##dataset contains data from various years so have to filter out 
+# using the year 2016 seemed like a good year to use since the datasets start 
+# from different years depending on country so 2016 is recent enough to have full data
+life_V_gdpDF = yearSelection(life_V_gdpDF,2016)
+
+#creation of scatter plot
+createScatterPlot(life_V_gdpDF,'GDP per capita', 'Life Expectancy','GDP vs Life Expectancy',
+                  'GDP per Capita', 'Life Expectancy')
