3415439dae7a49c115dd2f8f386377ece50e0986
victorious04 <victoriaadegbenla@gmail.com>
2025-01-04 00:14:02 +0000

    start of creation of visuals python file and creation of unit tests

--- DAT5901Visuals.py
+++ DAT5901Visuals.py
@@ -3,5 +3,40 @@ import pandas as pd
 import matplotlib as plt
 import numpy as np
 
-#test statement
-print("hello world!")
+###Functions that may be used often/speed up process
+
+#function to import data into program
+def importDF(filePath):
+    df = pd.read_csv(filePath)
+    return df
+
+#merges 2 datasets together 
+def mergeData(dataSet1, dataSet2,mergeCols,type):
+    merged = dataSet1.merge(dataSet2, on=mergeCols, how=type)
+    return merged
+
+#creation of a scatter plot
+def createScatterPlot(dataSet,xdata,ydata,title,xlabel,ylabel):
+    plt.scatter(dataSet[xdata], dataSet[ydata])
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.show()
+    return
+
+#saves visuals to be later used
+def saveVisual(outputpath):
+    plt.savefig(outputpath)
+    return
+##END OF FUNCTIONS
+
+
+#MAIN PROGRAM
+lifeDf = importDF('life-expectancy.csv')
+gdpDF = importDF('gdp-per-capita-worldbank.csv')
+
+#check that file has imported properly and see the layout
+print(lifeDf.head())
+print(gdpDF.head())
+
+
--- UnitTest.py
+++ UnitTest.py
@@ -1,6 +1,10 @@
+##THIS MAY NEED TO BE CHANGED
+
 ##importing python libraries needed##
 import unittest
 import pandas as pd
+import matplotlib as plt
+import os
 
 '''
 for this visualisation, the following functions for continuos testing 
@@ -12,23 +16,59 @@ seem to be the best to use. being:
 In total, there should be about 4 test functions overall. 
 '''
 
-##the data files will be called in quite offten so saves time making
-#them global variables
-gdpDf = pd.read_csv('gdp-per-capita-worldbank.csv')
-lifeDf = pd.read_csv('life-expectancy.csv')
+##using mock data as it helps to speed up testing 
+#and is also independent of external data
+#global as they are used often so easier to call
+gdpDf = pd.DataFrame({
+    'Entity':['UK','France','Spain'],
+    'Code':['UK','FRA','SPA'],
+    'Year':[2016,2016,2016],
+    'GDP per capita':[6000,4000,2000]
+})
+
+lifeDf = pd.DataFrame({
+    'Entity':['UK','France','Spain'],
+    'Code':['UK','FRA','SPA'],
+    'Year':[2016,2016,2016],
+    'Life Expectancy':[75,72,70]
+})
 
 class DataAnalysisTestFunctions(unittest.TestCase):
-    ##1. Column Check
+
+    ##1. Column Check - check that all necessary columns are present
     def columnPresenceCheck(self):
         #list of columns that need to bein each file in order to continue
         requiredDGPColumns = ['Entity','Year','GDP per capita']
         requiredLifeColumns = ['Entity','Year','Life Expectancy']
 
         #checks that every column in the required array is present
-        #if not, a message is returned
+        #if not, a message is returned to say which is missing
         for column in requiredDGPColumns:
             assert column in gdpDf.columns, f"Missing column for GDP: {column}"
         for column in requiredLifeColumns:
             assert column in lifeDf.columns, f"Missing column for Life Expectancy: {column}"
 
+    ##2. Merge check - see that the merge can be successfully performed
+    def test_MergeCheck(self):
+        #inner merge of the 2 datasets
+        mergedData = gdpDf.merge(lifeDf, on=['Country','Year'], how='inner')
 
+        #check that there is data after the merge, if not, error returns
+        self.assertFalse(mergedData.empty, "The merge was unsuccessful: Empty")
+        #checks that the number of columns are equal in column number, if not, error returns
+        self.assertEqual(len(mergedData.columns), len(gdpDf.columns) + 1, "Not all columns included in merge")
+
+    ##3a. checks that a visual can be saved successfully
+    def visualSavingCheck(self):
+        #creation of tester - in this case a bar chart
+        plt.bar(gdpDf['Entity'], gdpDf['GDP per capita'], color='blue')
+        plt.xlabel('Country')
+        plt.ylabel('GDP per Capita')
+        plt.title('GDP per Capita by Country')
+        plt.savefig('testerBarChart.png')
+        plt.close()
+
+        #tests that the file of that name exists, if not, erorr message returns
+        self.assertTrue(os.path.exists('testerBarChart.png'), 'File was not saved correctly')
+        #removes the file after test is completed
+        os.remove('testerBarChart.png')
